0da26878136e2550126122fd591b025393a6d68b
Blake Worsley <blakeworsley@gmail.com>
2016-10-31 12:59:29 -0600

    all of the visualizations in one

--- src/StackedBarGraph.js
+++ src/StackedBarGraph.js
@@ -1,41 +1,43 @@
 import React from 'react';
 
-const BarGraph = ({ width, height, colors, children }) => {
-  const barWidth = width / children.length;
-  const tallest = Math.max(...children.map(d => d.props.value));
-
-  const rects = children.map((dataPoint, index) => {
-    const barHeight = height / tallest * dataPoint.props.value;
-    return (
-      <rect
-        x={barWidth * index}
-        y={height - barHeight}
-        width={barWidth}
-        height={barHeight}
-        key={index}
-        fill={colors[index % colors.length]}
-      />
-    );
+const StackedBarGraph = ({ width, height, children, colors }) => {
+  const viewBox = `0 0 ${width} ${height}`;
+  const add = (a, b) => a + b;
+  const values = children.map(c => c.props.value);
+  const sum = values.reduce(add, 0);
+  const widths = values.map(c => width / sum * c);
+  const startingPoints = values.map((value, index) => {
+    return widths.slice(0, index).reduce(add, 0);
   });
+  const rects = children.map((datapoint, index) => (
+  <rect
+    x={startingPoints[index]}
+    y={0}
+    width={widths[index]}
+    height={height}
+    key={index}
+    fill={colors[index % colors.length]}
+  />
+));
 
   return (
-    <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg">
+    <svg width={width} height={height} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
       <g>
         {rects}
       </g>
     </svg>
   );
-}
-
-BarGraph.defaultProps = {
-  width: 600,
-  height: 400,
-  colors: ['#D0011B', '#50E3C2', '#F8E71C', '#4A90E2']
 };
 
-BarGraph.propTypes = {
+StackedBarGraph.propTypes = {
   width: React.PropTypes.number.isRequired,
   height: React.PropTypes.number.isRequired
 };
 
-export default BarGraph;
+StackedBarGraph.defaultProps = {
+  width: 600,
+  height: 40,
+  colors: ['#D0011B', '#50E3C2', '#F8E71C', '#4A90E2']
+};
+
+export default StackedBarGraph;
